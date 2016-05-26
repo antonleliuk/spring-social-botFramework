2da39a9d074ef7c1e261c0fac85c77b884f2db50
Anton Leliuk <a.leluk@profitsoft.ua>
2016-05-26 19:53:34 +0300

    Test request

--- examples/src/main/java/com/skype/controller/EchoController.java
+++ examples/src/main/java/com/skype/controller/EchoController.java
@@ -1,10 +1,11 @@
 package com.skype.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.social.skypeBot.api.SkypeBot;
 import org.springframework.social.skypeBot.api.data.to.Message;
-import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,10 +21,12 @@ public class EchoController {
     private SkypeBot skypeBot;
 
     @RequestMapping(value = "/chat", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void chat(@RequestBody Object fromSkype){
-//        System.out.println(fromSkype);
-        Message toSkype = new Message();
-        toSkype.setContent(ObjectUtils.nullSafeToString(fromSkype));
-        skypeBot.sendMessage("8:anton_leliuk", toSkype);
+    public void chat(@RequestBody List<org.springframework.social.skypeBot.api.data.from.Message> messages){
+        for (org.springframework.social.skypeBot.api.data.from.Message message : messages) {
+            Message toSkype = new Message();
+            toSkype.setContent(message.getContent());
+            skypeBot.sendMessage(message.getFrom(), toSkype);
+        }
+
     }
 }
