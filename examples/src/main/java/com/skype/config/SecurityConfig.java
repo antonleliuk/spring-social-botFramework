824d545044b6c66ff3bfa885bd1befc6e0ca8f53
Anton Leliuk <a.leluk@profitsoft.ua>
2017-02-05 15:14:45 +0200

    #2

--- examples/src/main/java/com/skype/config/SecurityConfig.java
+++ examples/src/main/java/com/skype/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.skype.config;
 
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -8,4 +9,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  */
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+    }
 }
